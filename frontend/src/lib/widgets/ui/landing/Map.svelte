9a6f3467f28580b5442f2e94273979f9c1ec9ed5
DarkDev <darkdev_brand@riseup.net>
2024-01-15 09:10:29 +0200

    upd layout for sm

--- frontend/src/lib/widgets/ui/landing/Map.svelte
+++ frontend/src/lib/widgets/ui/landing/Map.svelte
@@ -76,6 +76,7 @@
 
 	function handleMarkerClick(point) {
 		selectedLocation = point;
+		shortDesc = `${selectedLocation.desc.slice(0, 130)}...`;
 
 		map.flyTo({
 			center: point.coordinates,
@@ -96,38 +97,57 @@
 	};
 
 	let showLocation = false;
+	let showFull = false;
+
+	$: shortDesc = 'Tap on any point on the map.';
 
 	export let points;
 </script>
 
-<section class="flex min-h-screen flex-row  ">
+<section class="flex min-h-screen  flex-col-reverse  md:flex-row lg:flex-row">
 	<div id="map" class=" w-full">
 		<!--  -->
 	</div>
 
-	<div class="flex h-screen  w-[450px] flex-col bg-neutral-200">
-		<div class="flex h-40 w-full items-center justify-center bg-black bg-neutral-900 py-20 px-4">
-			<h3 class="font-oswald-normal text-xl uppercase ">Места уничтожения рома</h3>
+	<div class="flex lg:h-screen md:h-screen h-auto  lg:w-[450px] md:w-[450px] flex-col bg-neutral-200">
+		<div class="flex lg:h-40 md:h-40 h-20 w-full items-center justify-center bg-black bg-neutral-900 py-6 lg:px-4 md:px-4 px-2">
+			<h3 class="font-oswald-normal text-md uppercase md:text-xl lg:text-xl ">
+				Места уничтожения рома
+			</h3>
 		</div>
 		<div
-			class="prose-blue prose h-auto w-full border-b border-b-2 border-neutral-900 bg-neutral-100 px-6 pb-6 pt-10 text-neutral-900"
+			class="prose-blue prose h-auto w-full border-b border-b-2 border-neutral-900 bg-neutral-100 px-2 pt-2 text-neutral-900  md:px-4 lg:px-6"
 		>
-			<h3 class="font-oswald-normal text-2xl text-neutral-900">{selectedLocation.title}</h3>
-			<p class="font-notoSans-normal mb-8 text-sm text-neutral-900">{selectedLocation.desc}</p>
-			<a
-				href={selectedLocation.link}
-				target="_blank"
-				class="inline-flex flex-grow items-center text-left no-underline "
-			>
-				<span
-					class="pr-12 text-xs font-semibold uppercase tracking-widest text-neutral-800 hover:text-neutral-600"
+			<h3 class="font-oswald-normal mb-0 text-xl text-neutral-900 md:text-2xl lg:text-2xl">
+				{selectedLocation.title}
+			</h3>
+			<p class="font-notoSans-normal mb-2 h-auto text-sm text-neutral-900">
+				{showFull ? selectedLocation.desc : shortDesc}
+			</p>
+
+			<div class="flex flex-row flex-wrap pb-2 ">
+				<button
+					class="btn-neutral btn-active btn  "
+					on:click={() => {
+						showFull = !showFull;
+					}}>Show</button
 				>
-					more info
-				</span>
-				<span class="ml-auto text-neutral-800 hover:text-neutral-600">»</span>
-			</a>
+
+				<a
+					href={selectedLocation.link}
+					target="_blank"
+					class="inline-flex flex-grow items-center text-left no-underline "
+				>
+					<span
+						class="pl-20 text-xs font-semibold uppercase tracking-widest text-neutral-800 hover:text-neutral-600"
+					>
+						more info
+					</span>
+					<span class="ml-auto text-neutral-800 hover:text-neutral-600">»</span>
+				</a>
+			</div>
 		</div>
-		<div class="flex max-h-[600] flex-col items-center overflow-y-scroll bg-white px-4 py-4">
+		<div class="flex lg:max-h-[600px] max-w-sm md:max-h-[600px] max-h-[270px] flex-col items-center overflow-y-scroll bg-white px-4 py-2">
 			{#each points as location}
 				<div
 					on:click={() => {
@@ -135,7 +155,7 @@
 						handleMarkerClick(location);
 						// showLocation = !showLocation;
 					}}
-					class="mx-auto my-2 w-full rounded-md bg-neutral-300 px-8 py-6"
+					class="mx-auto my-2 lg:my-2 md:my-2 my-1 w-full rounded-md bg-neutral-300 px-8 lg:py-6 md:py-6 py-4"
 				>
 					<h3 class="font-notoSans-normal text-sm text-neutral-900">{location.title}</h3>
 				</div>
@@ -148,5 +168,13 @@
 	#map {
 		height: 100vh;
 		width: 100%;
+	}
+
+	@media screen and (max-width: 450px) {
+		#map {
+			max-height: 260px;
+			overflow: hidden;
+			width: 100%;
+		}
 	}
 </style>
