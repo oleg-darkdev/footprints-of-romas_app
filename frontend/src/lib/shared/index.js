047c38267d1b3315efd308e6eb5dfe25d1902c78
DarkDev <darkdev_brand@riseup.net>
2024-01-23 18:48:38 +0200

    upd api

--- frontend/src/lib/shared/index.js
+++ frontend/src/lib/shared/index.js
@@ -1,4 +1,5 @@
 import points from './data/locations';
 import PointerContent from './ui/PointerContent.svelte';
+import LogoText from './ui/LogoText.svelte';
 
-export { points, PointerContent };
+export { points, PointerContent, LogoText };
