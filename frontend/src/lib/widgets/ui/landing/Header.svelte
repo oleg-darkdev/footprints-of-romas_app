48f0d51cbad40be3c63691377dba5c95c1ff1d5b
DarkDev <darkdev_brand@riseup.net>
2024-01-23 18:48:48 +0200

    refactoring logo

--- frontend/src/lib/widgets/ui/landing/Header.svelte
+++ frontend/src/lib/widgets/ui/landing/Header.svelte
@@ -1,7 +1,7 @@
-<header class="w-full bg-white py-6">
-	<h1
-		class="font-oswald-bold mx-4 max-w-[270px] text-xl uppercase  text-neutral-900   md:ml-20 lg:ml-20"
-	>
-		Геноцид рома во время Второй мировой войны в Беларуси 1941-1944 гг
-	</h1>
+<script>
+	import { LogoText } from '$lib/shared';
+</script>
+
+<header class="w-full bg-white py-6  md:pl-20 lg:pl-20">
+	<LogoText textColor="text-neutral-900" />
 </header>
