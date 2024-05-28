5719a3d1e3ef939f1337f4a1cf6f3bb812ea6092
DarkDev <darkdev_brand@riseup.net>
2024-05-28 21:55:11 +0300

    add aliases

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,10 +3,17 @@ import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-
-  kit: {
-    adapter: adapter()
-  }
+	kit: {
+		adapter: adapter()
+	},
+	alias: {
+		$entities: 'src/lib/entities',
+		$features: 'src/lib/features',
+		$processes: 'src/lib/processes',
+		$widgets: 'src/lib/widgets',
+		$stores: 'src/lib/shared/stores',
+		$shared: 'src/lib/shared'
+	}
 };
 
 export default config;
